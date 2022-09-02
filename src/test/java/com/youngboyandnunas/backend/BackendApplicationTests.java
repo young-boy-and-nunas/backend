c28ea17d9063c6d2a600a0b0f50ea10e6ecbaa24
dohyeon5626 <kodohyeon71@gmail.com>
2022-09-02 23:48:19 +0900

    Add base project settings

---
+++ src/test/java/com/youngboyandnunas/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.youngboyandnunas.backend;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	@DisplayName("프로젝트 로드")
+	void contextLoads() {
+	}
+
+}
