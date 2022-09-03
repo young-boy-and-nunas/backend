88dedb2ee48e7f9a8cc8ebd6ffd49d1061050a7d
YIHYUN HA <hihyun16@gmail.com>
2022-09-04 02:51:35 +0900

    Add imgFile exception handling

--- src/main/java/com/youngboyandnunas/backend/domain/worry/dto/CreateWorryRequestDto.java
+++ src/main/java/com/youngboyandnunas/backend/domain/worry/dto/CreateWorryRequestDto.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class CreateWorryRequestDto {
 
     private String contents;
-    private MultipartFile imgUrl;
+    private MultipartFile imgFile;
     private Long userSeq;
 
 }
