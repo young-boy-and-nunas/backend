94eba4fb72cc09e326615deb38d87879ac30d87d
zzarbttoo <qodbwls70@naver.com>
2022-09-03 16:58:31 +0900

    s3 error

---
+++ src/main/java/com/youngboyandnunas/backend/controller/rest/AutoCheerRestController.java
@@ -0,0 +1,10 @@
+package com.youngboyandnunas.backend.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutoCheerRestController {
+
+
+
+}
