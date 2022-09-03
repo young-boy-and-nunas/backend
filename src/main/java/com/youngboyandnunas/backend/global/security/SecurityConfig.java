32427e9365006218fb2ed850077fb7937af3b7d8
dohyeon5626 <kodohyeon71@gmail.com>
2022-09-03 20:13:01 +0900

    swagger security settings

--- src/main/java/com/youngboyandnunas/backend/global/security/SecurityConfig.java
+++ src/main/java/com/youngboyandnunas/backend/global/security/SecurityConfig.java
@@ -29,6 +29,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().exceptionHandling()
                 .and().authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        /* swagger v3 */
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**").permitAll()
                 .antMatchers("/sign-up*", "/sign-up/*").permitAll()
                 .anyRequest().authenticated()
                 .and().apply(new FilterConfig(jwtTokenProvider));
