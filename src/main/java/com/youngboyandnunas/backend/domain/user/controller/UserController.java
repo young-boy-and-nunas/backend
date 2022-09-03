27f4919534805b0d11deb84b933f8f31b991af1d
dohyeon5626 <kodohyeon71@gmail.com>
2022-09-03 21:47:28 +0900

    Add get user api

--- src/main/java/com/youngboyandnunas/backend/domain/user/controller/UserController.java
+++ src/main/java/com/youngboyandnunas/backend/domain/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.youngboyandnunas.backend.domain.user.controller;
 
+import com.youngboyandnunas.backend.domain.user.dto.UserResponse;
 import com.youngboyandnunas.backend.domain.user.service.UserService;
 import com.youngboyandnunas.backend.global.security.service.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +17,15 @@ public class UserController {
     private final AuthenticationFacade authenticationFacade;
     private final UserService userService;
 
+    @GetMapping("/user")
+    public UserResponse getUser() {
+        return userService.getUser(authenticationFacade.getUserId());
+    }
+
     @DeleteMapping("/user")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void withThrowUser() {
-        Long id = authenticationFacade.getUserId();
-        userService.withThrowUser(id);
+        userService.withThrowUser(authenticationFacade.getUserId());
     }
 
 }
